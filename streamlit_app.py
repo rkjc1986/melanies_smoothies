22c9dea5eeae538990a9942beaac3707afb0f92b
rkjc1986 <62369234+rkjc1986@users.noreply.github.com>
2024-04-08 19:16:45 +0530

    Update streamlit_app.py
    Requests added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+
 # Write directly to the app
 st.title(":cup_with_straw  Customize your Smoothie: :cup_with_straw")
 st.write(
@@ -57,3 +58,6 @@ if ingredients_list:
 
 
     
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
